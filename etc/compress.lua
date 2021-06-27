c69f45ecd85b185691654dd51fd653981e3fd039
Jean-Patrick Guerrero <jeanpatrick.guerrero@gmail.com>
2021-06-27 04:56:38 +0200

    Also compress moreblocks micronodes

--- etc/compress.lua
+++ etc/compress.lua
@@ -117,6 +117,186 @@ local to_compress = {
 	},
 }
 
+local circular_saw_names = {
+	{"micro", "_1"},
+	{"panel", "_1"},
+	{"micro", "_2"},
+	{"panel", "_2"},
+	{"micro", "_4"},
+	{"panel", "_4"},
+	{"micro", ""},
+	{"panel", ""},
+
+	{"micro", "_12"},
+	{"panel", "_12"},
+	{"micro", "_14"},
+	{"panel", "_14"},
+	{"micro", "_15"},
+	{"panel", "_15"},
+	{"stair", "_outer"},
+	{"stair", ""},
+
+	{"stair", "_inner"},
+	{"slab", "_1"},
+	{"slab", "_2"},
+	{"slab", "_quarter"},
+	{"slab", ""},
+	{"slab", "_three_quarter"},
+	{"slab", "_14"},
+	{"slab", "_15"},
+
+	{"slab", "_two_sides"},
+	{"slab", "_three_sides"},
+	{"slab", "_three_sides_u"},
+	{"stair", "_half"},
+	{"stair", "_alt_1"},
+	{"stair", "_alt_2"},
+	{"stair", "_alt_4"},
+	{"stair", "_alt"},
+	{"stair", "_right_half"},
+
+	{"slope", ""},
+	{"slope", "_half"},
+	{"slope", "_half_raised"},
+	{"slope", "_inner"},
+	{"slope", "_inner_half"},
+	{"slope", "_inner_half_raised"},
+	{"slope", "_inner_cut"},
+	{"slope", "_inner_cut_half"},
+
+	{"slope", "_inner_cut_half_raised"},
+	{"slope", "_outer"},
+	{"slope", "_outer_half"},
+	{"slope", "_outer_half_raised"},
+	{"slope", "_outer_cut"},
+	{"slope", "_outer_cut_half"},
+	{"slope", "_outer_cut_half_raised"},
+	{"slope", "_cut"},
+}
+
+local moreblocks_nodes = {
+	"coal_stone",
+	"wood_tile",
+	"iron_checker",
+	"circle_stone_bricks",
+	"cobble_compressed",
+	"plankstone",
+	"clean_glass",
+	"split_stone_tile",
+	"all_faces_tree",
+	"dirt_compressed",
+	"coal_checker",
+	"clean_glow_glass",
+	"tar",
+	"clean_super_glow_glass",
+	"stone_tile",
+	"cactus_brick",
+	"super_glow_glass",
+	"desert_cobble_compressed",
+	"copperpatina",
+	"coal_stone_bricks",
+	"glow_glass",
+	"cactus_checker",
+	"all_faces_pine_tree",
+	"all_faces_aspen_tree",
+	"all_faces_acacia_tree",
+	"all_faces_jungle_tree",
+	"iron_stone",
+	"grey_bricks",
+	"wood_tile_left",
+	"wood_tile_down",
+	"wood_tile_center",
+	"wood_tile_right",
+	"wood_tile_full",
+	"checker_stone_tile",
+	"iron_glass",
+	"iron_stone_bricks",
+	"wood_tile_flipped",
+	"wood_tile_offset",
+	"coal_glass",
+
+	"straw",
+
+	"stone",
+	"stone_block",
+	"cobble",
+	"mossycobble",
+	"brick",
+	"sandstone",
+	"steelblock",
+	"goldblock",
+	"copperblock",
+	"bronzeblock",
+	"diamondblock",
+	"tinblock",
+	"desert_stone",
+	"desert_stone_block",
+	"desert_cobble",
+	"meselamp",
+	"glass",
+	"tree",
+	"wood",
+	"jungletree",
+	"junglewood",
+	"pine_tree",
+	"pine_wood",
+	"acacia_tree",
+	"acacia_wood",
+	"aspen_tree",
+	"aspen_wood",
+	"obsidian",
+	"obsidian_block",
+	"obsidianbrick",
+	"obsidian_glass",
+	"stonebrick",
+	"desert_stonebrick",
+	"sandstonebrick",
+	"silver_sandstone",
+	"silver_sandstone_brick",
+	"silver_sandstone_block",
+	"desert_sandstone",
+	"desert_sandstone_brick",
+	"desert_sandstone_block",
+	"sandstone_block",
+	"coral_skeleton",
+	"ice",
+}
+
+local colors_moreblocks = table.copy(colors)
+insert(colors_moreblocks, "white")
+
+local moreblocks_mods = {
+	wool = colors_moreblocks,
+	moreblocks = moreblocks_nodes,
+}
+
+local t = {}
+
+for mod, v in pairs(moreblocks_mods) do
+for _, nodename in ipairs(v) do
+	t[nodename] = {}
+
+	for _, shape in ipairs(circular_saw_names) do
+		local to_add = true
+
+		if shape[1] == "slope" and shape[2] == "" then
+			to_add = nil
+		end
+
+		if to_add then
+			insert(t[nodename], fmt("%s_%s%s", shape[1], nodename, shape[2]))
+		end
+	end
+
+	local slope_name = fmt("slope_%s", nodename)
+
+	to_compress[fmt("%s:%s", mod, slope_name)] = {
+		replace = slope_name,
+		by = t[nodename]
+	}
+end
+end
+
 local compressed = {}
 
 for k, v in pairs(to_compress) do
