5c96ede065644c2cc8762b34955e078b10f5fc8c
Jean-Patrick Guerrero <jeanpatrick.guerrero@gmail.com>
2021-07-05 23:38:43 +0200

    Add confirmation dialog on inventory clearing

--- init.lua
+++ init.lua
@@ -2301,20 +2301,21 @@ local function init_data(player, info)
 end
 
 local function reset_data(data)
-	data.filter      = ""
-	data.expand      = ""
-	data.pagenum     = 1
-	data.rnum        = 1
-	data.unum        = 1
-	data.scrbar_rcp  = 1
-	data.scrbar_usg  = 1
-	data.query_item  = nil
-	data.recipes     = nil
-	data.usages      = nil
-	data.export_rcp  = nil
-	data.export_usg  = nil
-	data.alt_items   = nil
-	data.items       = data.items_raw
+	data.filter        = ""
+	data.expand        = ""
+	data.pagenum       = 1
+	data.rnum          = 1
+	data.unum          = 1
+	data.scrbar_rcp    = 1
+	data.scrbar_usg    = 1
+	data.query_item    = nil
+	data.recipes       = nil
+	data.usages        = nil
+	data.export_rcp    = nil
+	data.export_usg    = nil
+	data.alt_items     = nil
+	data.confirm_trash = nil
+	data.items         = data.items_raw
 
 	if data.current_itab > 1 then
 		sort_by_category(data)
@@ -2531,6 +2532,18 @@ local function get_inventory_fs(player, data, fs)
 		fs("image_button", i + 3.447 - (i * 0.4), 11.43, 0.35, 0.35, "", btn_name, "")
 		fs(fmt("tooltip[%s;%s]", btn_name, tooltip))
 	end
+
+	if data.confirm_trash then
+		fs("style_type[box;colors=#999,#999,#808080,#808080]")
+
+		for _ = 1, 3 do
+			fs("box", 2.95, 10.75, 4.35, 0.5, "")
+		end
+
+		fs("label", 3.1, 11, "Confirm trash?")
+		fs("image_button", 5.2, 10.75, 1, 0.5, "", "confirm_trash_yes", "Yes")
+		fs("image_button", 6.3, 10.75, 1, 0.5, "", "confirm_trash_no", "No")
+	end
 end
 
 i3.new_tab {
@@ -2602,9 +2615,16 @@ i3.new_tab {
 		end
 
 		if fields.trash then
-			local inv = player:get_inventory()
-			inv:set_list("main", {})
-			inv:set_list("craft", {})
+			data.confirm_trash = true
+
+		elseif fields.confirm_trash_yes or fields.confirm_trash_no then
+			if fields.confirm_trash_yes then
+				local inv = player:get_inventory()
+				inv:set_list("main", {})
+				inv:set_list("craft", {})
+			end
+
+			data.confirm_trash = nil
 
 		elseif fields.compress then
 			compress_items(player)
