2fadcdefddeb3bed3a98fbaf441ca6e787b4ad70
Jean-Patrick Guerrero <jeanpatrick.guerrero@gmail.com>
2021-12-25 17:01:25 +0100

    Fix variable name

--- src/hud.lua
+++ src/hud.lua
@@ -34,7 +34,7 @@ local function init_hud(player)
 		},
 	}
 
-	if not i3.legacy_inventory then
+	if not i3.settings.legacy_inventory then
 		core.after(0, function()
 			player:hud_set_hotbar_itemcount(i3.settings.hotbar_len)
 			player:hud_set_hotbar_image"i3_hotbar.png"
